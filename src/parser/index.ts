45575b787c77b5967619d157b4ea6764841c7146
su-u <aaaiiiuuu35@gmail.com>
2020-07-25 17:37:02 +0900

    Refactor: format

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,23 +1,23 @@
 import { ChiboParser, JsonLexer } from './parser';
-import * as fs from "fs-extra";
+import * as fs from 'fs-extra';
 
 // ONLY ONCE
 const parser = new ChiboParser();
 
 function parseInput(text) {
-  const lexResult = JsonLexer.tokenize(text)
+  const lexResult = JsonLexer.tokenize(text);
   // setting a new input will RESET the parser instance's state.
-  parser.input = lexResult.tokens
+  parser.input = lexResult.tokens;
   // any top level rule may be used as an entry point
   const ast = parser.Program();
 
   if (parser.errors.length > 0) {
-    throw new Error("sad sad panda, Parsing errors detected")
+    throw new Error('sad sad panda, Parsing errors detected');
   }
 
   return ast;
 }
 
 const inputText = fs.readFileSync(process.argv[2], 'utf-8');
-const ast = parseInput(inputText)
+const ast = parseInput(inputText);
 console.log(JSON.stringify(ast, undefined, 2));
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,41 +1,39 @@
 import { Lexer, CstParser, createToken } from 'chevrotain';
 
 const StringLiteral = createToken({
-  name: "StringLiteral", pattern: /"(:?[^\\"\n\r]+|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/
+  name: 'StringLiteral',
+  pattern: /"(:?[^\\"\n\r]+|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/,
 });
 const NumberLiteral = createToken({
-  name: "NumberLiteral", pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/
+  name: 'NumberLiteral',
+  pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/,
 });
 const WhiteSpace = createToken({
-  name: "WhiteSpace",
+  name: 'WhiteSpace',
   pattern: /\s+/,
-  group: Lexer.SKIPPED
+  group: Lexer.SKIPPED,
 });
 
-const allTokens = [
-  WhiteSpace,
-  NumberLiteral,
-  StringLiteral,
-]
+const allTokens = [WhiteSpace, NumberLiteral, StringLiteral];
 
 const jsonTokens = [WhiteSpace, NumberLiteral, StringLiteral];
 
-export const JsonLexer = new Lexer(allTokens)
+export const JsonLexer = new Lexer(allTokens);
 
 export class ChiboParser extends CstParser {
   constructor() {
-    super(jsonTokens)
+    super(jsonTokens);
     this.performSelfAnalysis();
   }
 
-  public Program = this.RULE("Program", () => {
-      this.SUBRULE(this.value)
-    });
+  public Program = this.RULE('Program', () => {
+    this.SUBRULE(this.value);
+  });
 
-   private value = this.RULE("value", () => {
-      this.OR([
-        {ALT: () => this.CONSUME(StringLiteral)},
-        {ALT: () => this.CONSUME(NumberLiteral)},
-      ]);
-    });
+  private value = this.RULE('value', () => {
+    this.OR([
+      { ALT: () => this.CONSUME(StringLiteral) },
+      { ALT: () => this.CONSUME(NumberLiteral) },
+    ]);
+  });
 }
