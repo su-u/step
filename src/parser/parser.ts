df517bbf7b82b0c5ef05da3b0b15b0c21ce6f863
su-u <aaaiiiuuu35@gmail.com>
2020-07-27 20:35:30 +0900

    足し算できない

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -25,36 +25,72 @@ const Identifier = createToken({
   pattern: /[a-zA-z][0-9a-zA-Z]+/,
 });
 
-const LBracket = createToken({name: "LBrackets", pattern: /\(/, label: '('});
-const RBracket = createToken({name: "RBrackets", pattern: /\)/, label: ')'});
-const LCurly = createToken({name: "LCurly", pattern: /{/, label: '{'});
-const RCurly = createToken({name: "RCurly", pattern: /}/, label: '}'});
-const LSquare = createToken({name: "LSquare", pattern: /\[/, label: '['});
-const RSquare = createToken({name: "RSquare", pattern: /]/, label: ']'});
+const Tokens = [StringLiteral, NumberLiteral, WhiteSpace, BoolLiteral, Identifier];
 
-const SubstitutionOperator = createToken( {name: "SubstitutionOperator", pattern: Lexer.NA});
-const ToRight = createToken({name: "ToRight", pattern: /(?!<)->/, categories: SubstitutionOperator})
-const ToLeft = createToken({name: "ToLeft", pattern: /<-(?!>)/, categories: SubstitutionOperator})
+const LBracket = createToken({ name: 'LBrackets', pattern: /\(/, label: '(' });
+const RBracket = createToken({ name: 'RBrackets', pattern: /\)/, label: ')' });
+const LCurly = createToken({ name: 'LCurly', pattern: /{/, label: '{' });
+const RCurly = createToken({ name: 'RCurly', pattern: /}/, label: '}' });
+const LSquare = createToken({ name: 'LSquare', pattern: /\[/, label: '[' });
+const RSquare = createToken({ name: 'RSquare', pattern: /]/, label: ']' });
 
-const AdditionOperator = createToken({name: "AdditionOperator", pattern: Lexer.NA});
-const Plus = createToken({name: "Plus", pattern: /\+/, categories: AdditionOperator});
-const Minus = createToken({name: "Minus", pattern: /(?!<)-(?!>)/, categories: AdditionOperator});
+const BracketTokens = [LBracket, RBracket, LCurly, RCurly, LSquare, RSquare];
 
-const MultiplicationOperator = createToken({name: "MultiplicationOperator", pattern: Lexer.NA});
-const Multi = createToken({name: "Multi", pattern: /\*/, categories: MultiplicationOperator});
-const Div = createToken({name: "Div", pattern: /\//, categories: MultiplicationOperator});
+// const SubstitutionOperator = createToken({ name: 'SubstitutionOperator', pattern: Lexer.NA });
+const ToRight = createToken({
+  name: 'ToRight',
+  pattern: /(?!<)->/,
+});
+const ToLeft = createToken({
+  name: 'ToLeft',
+  pattern: /<-(?!>)/,
+});
 
-const Pipe = createToken({name: "Pipe", pattern: /(?!-)>>/})
+const AdditionOperator = createToken({ name: 'AdditionOperator', pattern: Lexer.NA });
+const Plus = createToken({ name: 'Plus', pattern: /\+/, categories: AdditionOperator });
+const Minus = createToken({ name: 'Minus', pattern: /(?!<)-(?!>)/, categories: AdditionOperator });
 
-const RelationalOperator = createToken({name: "RelationalOperator", pattern: Lexer.NA});
-const AmountMore = createToken({name: "AmountMore", pattern: /<=/, categories: RelationalOperator});
-const AmountLess = createToken({name: "AmountLess", pattern: />=/, categories: RelationalOperator});
-const OverThan = createToken({name: "OverTha", pattern: /</, categories: RelationalOperator});
-const LessThan = createToken({name: "LessThan", pattern: />/, categories: RelationalOperator});
-const Equal = createToken({name: "Equal", pattern: /=/, categories: RelationalOperator});
+const MultiplicationOperator = createToken({ name: 'MultiplicationOperator', pattern: Lexer.NA });
+const Multi = createToken({ name: 'Multi', pattern: /\*/, categories: MultiplicationOperator });
+const Div = createToken({ name: 'Div', pattern: /\//, categories: MultiplicationOperator });
 
+const OperatorTokens = [
+  ToRight,
+  ToLeft,
+  AdditionOperator,
+  Plus,
+  Minus,
+  MultiplicationOperator,
+  Multi,
+  Div,
+];
 
-const allTokens = [WhiteSpace, NumberLiteral, StringLiteral, Plus, Minus, Multi, Div, ToRight, ToLeft, Identifier];
+const RelationalOperator = createToken({ name: 'RelationalOperator', pattern: Lexer.NA });
+const AmountMore = createToken({
+  name: 'AmountMore',
+  pattern: /<=/,
+  categories: RelationalOperator,
+});
+const AmountLess = createToken({
+  name: 'AmountLess',
+  pattern: />=/,
+  categories: RelationalOperator,
+});
+const OverThan = createToken({ name: 'OverTha', pattern: /<(?!-)/, categories: RelationalOperator });
+const LessThan = createToken({ name: 'LessThan', pattern: /(?!-)>/, categories: RelationalOperator });
+const Equal = createToken({ name: 'Equal', pattern: /(?!<>)=/, categories: RelationalOperator });
+const Pipe = createToken({ name: 'Pipe', pattern: /(?!-)>>/ });
+
+const RelationalOperatorTokens = [
+  RelationalOperator,
+  AmountMore,
+  AmountLess,
+  OverThan,
+  LessThan,
+  Equal,
+];
+
+const allTokens = [...Tokens, ...BracketTokens, ...OperatorTokens, ...RelationalOperatorTokens];
 
 export const JsonLexer = new Lexer(allTokens);
 
@@ -67,105 +103,77 @@ export class ChiboParser extends CstParser {
     this.performSelfAnalysis();
   }
 
-    public Program = this.RULE('Program', () => {
-      this.MANY(() => {
-        this.SUBRULE(this.Substitution);
-      });
-    });
-
-    private Literal =  this.RULE('Literal', () => {
-      this.OR([
-        { ALT: () => this.CONSUME(StringLiteral) },
-        { ALT: () => this.CONSUME(NumberLiteral) },
-      ]);
-    });
-
-    private Expr = this.RULE('Expr', () => {
-      this.OR([
-        { ALT: () => this.SUBRULE(this.Substitution) },
-        { ALT: () => this.SUBRULE(this.Pipe) },
-      ]);
-    });
-
-    private Factor = this.RULE('Factor', () => {
+  public Program = this.RULE('Program', () => {
+    this.MANY(() => {
       this.OR([
         { ALT: () => {
+            this.SUBRULE(this.Pipe, { LABEL: 'left' });
+            this.CONSUME(ToRight);
+            this.SUBRULE2(this.To, {LABEL: 'right'});
+          } },
+        { ALT: () => {
+            this.SUBRULE(this.To, { LABEL: 'right' });
+            this.CONSUME(ToLeft);
+            this.SUBRULE2(this.RelationExpression, { LABEL: 'left' });
+          } },
+      ]);
+    });
+  });
+
+  private Term = this.RULE('Term', () => {
+    this.SUBRULE(this.Factor);
+    this.MANY(() => {
+      this.CONSUME(MultiplicationOperator);
+      this.SUBRULE2(this.Factor);
+    });
+  });
+
+  private Factor = this.RULE('Factor', () => {
+    this.OR([
+      { ALT: () => this.CONSUME(NumberLiteral) },
+      {
+        ALT: () => {
           this.CONSUME(LBracket);
           this.SUBRULE(this.RelationExpression);
           this.CONSUME(RBracket);
-        } },
-        { ALT: () => this.CONSUME(StringLiteral) },
-        { ALT: () => this.CONSUME(NumberLiteral) },
-        { ALT: () => this.CONSUME(Identifier) },
-      ]);
-    });
+        },
+      },
+      { ALT: () => this.CONSUME(StringLiteral) },
+      { ALT: () => this.CONSUME(Identifier) },
+    ]);
+  });
 
-    private Expression = this.RULE('Expression', () => {
-      this.MANY(() => {
-        this.OR([
-          {ALT: () => this.SUBRULE(this.Term)},
-          {ALT: () => this.SUBRULE2(this.BinaryExpression)},
-        ]);
-      });
-    });
+  private To = this.RULE('To', () => {
+    this.CONSUME(Identifier);
+  });
 
-    private Term = this.RULE('Term', () => {
-      this.SUBRULE(this.Literal)
-      this.CONSUME(MultiplicationOperator);
-      this.SUBRULE2(this.Literal)
-    });
-
-    private BinaryExpression = this.RULE('BinaryExpression', () => {
-      this.SUBRULE(this.Literal);
-      this.CONSUME(AdditionOperator);
-      this.SUBRULE2(this.Literal);
-    });
-
-    private Substitution = this.RULE('Substitution', () => {
-      this.OR([
-        { ALT: () =>       {
-            this.SUBRULE1(this.To, {LABEL: "right"});
-            this.CONSUME(ToLeft);
-            this.SUBRULE2(this.From, {LABEL: "left"});
-          }},
-        { ALT: () =>       {
-            this.SUBRULE3(this.From, {LABEL: "left"});
-            this.CONSUME(ToRight);
-            this.SUBRULE4(this.To, {LABEL: "right"});
-          }},
-      ]);
-    });
-
-    private To = this.RULE('To', () => {
-      this.CONSUME(Identifier);
-    });
-
-    private Pipe = this.RULE('Pipe', () => {
-      this.SUBRULE(this.From);
+  private Pipe = this.RULE('Pipe', () => {
+    this.SUBRULE(this.RelationExpression);
+    this.MANY(() => {
       this.CONSUME(Pipe);
-      this.SUBRULE2(this.From);
+      this.SUBRULE2(this.RelationExpression);
     });
+  });
 
-    private From = this.RULE('From', () => {
+  private RelationExpression = this.RULE('RelationExpression', () => {
+    this.SUBRULE(this.Expression);
+    this.MANY(() => {
       this.OR([
-        { ALT: () => this.CONSUME(BoolLiteral) },
-        { ALT: () => this.CONSUME(Identifier) },
-        { ALT: () => this.CONSUME(StringLiteral) },
-        { ALT: () => this.CONSUME(NumberLiteral) },
+        { ALT: () => this.CONSUME(AmountMore) },
+        { ALT: () => this.CONSUME(AmountLess) },
+        { ALT: () => this.CONSUME(OverThan) },
+        { ALT: () => this.CONSUME(LessThan) },
+        { ALT: () => this.CONSUME(Equal) },
       ]);
+      this.SUBRULE2(this.Expression);
     });
+  });
 
-    private RelationExpression = this.RULE('RelationExpression', () =>{
-      this.MANY(() => {
-        this.SUBRULE(this.Expression);
-        this.OR([
-          {ALT: () => this.CONSUME(AmountMore)},
-          {ALT: () => this.CONSUME(AmountLess)},
-          {ALT: () => this.CONSUME(OverThan)},
-          {ALT: () => this.CONSUME(LessThan)},
-          {ALT: () => this.CONSUME(Equal)},
-        ]);
-        this.SUBRULE2(this.Expression);
-      });
+  private Expression = this.RULE('Expression', () => {
+    this.SUBRULE(this.Term)
+    this.MANY(() => {
+      this.CONSUME(AdditionOperator);
+      this.SUBRULE2(this.Term)
     });
+  });
 }
