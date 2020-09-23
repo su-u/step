3a8ad3a44ea00ace80e233bf8b90e132886cc0a0
su-u <aaaiiiuuu35@gmail.com>
2020-09-23 23:52:24 +0900

    Create expression.ts

---
+++ src/interpriterRules/expression.ts
@@ -0,0 +1,15 @@
+import { IInterpreterRules } from './types';
+import { interpreter } from '../interpreter';
+
+export const expression = ({ ast }: IInterpreterRules) => {
+  const result =  Object.keys(ast.children).map((rule) => {
+    if (ast.children[rule][0].image === undefined) {
+      const a = interpreter(ast.children[rule][0]);
+      const b = interpreter(ast.children[rule][1]);
+      return [...a, ...b];
+    } else {
+      return ast.children[rule][0].image;
+    }
+  });
+  return result;
+};
