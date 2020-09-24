5b4e7c1a16ff1111ff3d0beda509a6ee9bba67dc
su-u <aaaiiiuuu35@gmail.com>
2020-09-24 23:38:13 +0900

    Create relationExpression.ts

---
+++ src/interpriterRules/relationExpression.ts
@@ -0,0 +1,11 @@
+import { IInterpreterRules } from './types';
+import { interpreter } from '../interpreter';
+
+
+export const relationExpression = ({ ast }: IInterpreterRules) => {
+  Object.keys(ast.children).forEach((rule) => {
+    for (let line of ast.children[rule]) {
+      interpreter(line);
+    }
+  });
+};
