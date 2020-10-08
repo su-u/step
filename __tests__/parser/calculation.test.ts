ac9d7574578f3d78686535199694d0b5afacf36e
su-u <aaaiiiuuu35@gmail.com>
2020-10-09 04:09:58 +0900

    Update calculation.test.ts

--- __tests__/parser/calculation.test.ts
+++ __tests__/parser/calculation.test.ts
@@ -21,10 +21,34 @@ describe('演算', () => {
     `;
     expect(sourceParse(source)).toMatchSnapshot();
   });
+
   test('除算', () => {
     const source = `
     1 / 1
     `;
     expect(sourceParse(source)).toMatchSnapshot();
+  });
+
+  describe('複雑', () => {
+    test('1', () => {
+      const source = `
+      1 + 1 + 1 + 1
+      `;
+      expect(sourceParse(source)).toMatchSnapshot();
+    });
+
+    test('2', () => {
+      const source = `
+      (1 + 1) * 3
+      `;
+      expect(sourceParse(source)).toMatchSnapshot();
+    });
+
+    test('3', () => {
+      const source = `
+      (1 + 1) / (1100 * 50)
+      `;
+      expect(sourceParse(source)).toMatchSnapshot();
+    });
   });
 });
