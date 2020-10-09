00f4fd0397bfabe4f1db9b210335c3191c9954d1
su-u <aaaiiiuuu35@gmail.com>
2020-10-09 22:30:18 +0900

    Feat; if

---
+++ __tests__/parser/if.test.ts
@@ -0,0 +1,22 @@
+import { sourceParse } from '@/util/test';
+
+describe('if', () => {
+  test('1', () => {
+    const source = `
+    if (x) {
+      x + x
+    }
+    `;
+    expect(sourceParse(source)).toMatchSnapshot();
+  });
+  test('2', () => {
+    const source = `
+    if (x) {
+      x + x
+    } else {
+      x - x
+    }
+    `;
+    expect(sourceParse(source)).toMatchSnapshot();
+  });
+});
