d0e823b20582019a3f301d14d0ea4d3f784a8ef6
su-u <aaaiiiuuu35@gmail.com>
2020-10-20 06:37:59 +0900

    Feat: 左辺代入

--- src/interpriterRules/assignment.ts
+++ src/interpriterRules/assignment.ts
@@ -5,13 +5,14 @@ import { variableManager } from '../manager';
 export const assignment = ({ ast }: IInterpreterRules) => {
   const key = Object.keys(ast.children)[0];
   if (key === 'ToRight') {
-    const from = interpreter(ast.children.ToRight[0].children.from[0]);
-    // console.log('f', from);
-    const toObject = ast.children.ToRight[0].children.to[0];
+    const obj = ast.children.ToRight[0];
+    const from = interpreter(obj.children.from[0]);
+    const toObject = obj.children.to[0];
     variableManager.assignment(toObject, from);
   } else {
-    // const from = interpreter(ast.children.from[0]);
-    // const op = ast.children.ToLeftToken[0];
-    // const to = interpreter(ast.children.to[0]);
+    const obj = ast.children.ToLeft[0];
+    const from = interpreter(obj.children.from[0]);
+    const toObject = obj.children.to[0];
+    variableManager.assignment(toObject, from);
   }
 };
