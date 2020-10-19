a87dc53079e40ad89382dc3a01d97d55cc823cc3
su-u <aaaiiiuuu35@gmail.com>
2020-10-19 18:25:08 +0900

    Create blockStatement.ts

---
+++ src/interpriterRules/blockStatement.ts
@@ -0,0 +1,10 @@
+import { IInterpreterRules } from './types';
+import { interpreter } from '../interpreter';
+
+export const blockStatement = ({ ast }: IInterpreterRules) => {
+  Object.keys(ast.children).forEach((rule) => {
+    for (let line of ast.children[rule]) {
+      interpreter(line);
+    }
+  });
+};
