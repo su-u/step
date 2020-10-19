31237bd60a9230fa9c2e67143ad5c4aa4f10b45c
su-u <aaaiiiuuu35@gmail.com>
2020-10-20 07:54:30 +0900

    Feat: 関数の登録

--- src/interpriterRules/functions.ts
+++ src/interpriterRules/functions.ts
@@ -4,5 +4,9 @@ import { functionManager } from '../manager';
 
 export const functionStatement = ({ ast }: IInterpreterRules) => {
   const functionName = ast.children.FunctionNameToken[0].image;
-  functionManager.assignment(functionName, [], {});
+  const funcArguments = Object.values(ast.children.arguments[0].children).map((x: any) => {
+    return x[0].image;
+  });
+  console.log('a', funcArguments);
+  functionManager.assignment(functionName, funcArguments, ast.children.Program[0]);
 };
