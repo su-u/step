2fe0f2f4163b3f583ee10e457b95651e97907f4c
su-u <aaaiiiuuu35@gmail.com>
2020-10-20 09:14:52 +0900

    Fix: 複数の引数に対応

--- src/interpriterRules/functions.ts
+++ src/interpriterRules/functions.ts
@@ -4,8 +4,8 @@ import { functionManager } from '../manager';
 
 export const functionStatement = ({ ast }: IInterpreterRules) => {
   const functionName = ast.children.FunctionNameToken[0].image;
-  const funcArguments = Object.values(ast.children.arguments[0].children).map((x: any) => {
-    return x[0].image;
+  const funcArguments = ast.children.arguments[0].children.Identifier.map((x: any) => {
+    return x.image;
   });
   functionManager.assignment(functionName, funcArguments, ast.children.Program[0]);
   return;
