3a4af2f683ecc1c1221f9e05ad474c7c3d9be10e
su-u <aaaiiiuuu35@gmail.com>
2020-10-22 03:25:08 +0900

    Create stringClass.ts

---
+++ src/class/stringClass.ts
@@ -0,0 +1,61 @@
+import { LiteralType } from '../types/literal';
+
+const TYPE = 'StringLiteral';
+
+type StringLiteralType = LiteralType<string>;
+
+export type StringObjectMethodType = {
+  '+': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '*': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '-': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '/': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '<': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '<=': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '>': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '>=': <T extends number>(obj: StringLiteralType, param: any) => any;
+  '~': <T extends number>(obj: StringLiteralType, param: any) => any;
+  to_s: <T extends number>(obj: StringLiteralType, param: any) => string;
+};
+
+export const StringClass: StringObjectMethodType = {
+  '+': (obj, param) => {
+    return {
+      name: TYPE,
+      image: obj.image + param.image,
+    };
+  },
+  '<': (obj, param) => {
+    return {
+      name: TYPE,
+      image: obj.image < param.image,
+    };
+  },
+  '<=': (obj, param) => {
+    return {
+      name: TYPE,
+      image: obj.image <= param.image,
+    };
+  },
+  '>': (obj, param) => {
+    return {
+      name: TYPE,
+      image: obj.image > param.image,
+    };
+  },
+  '>=': (obj, param) => {
+    return {
+      name: TYPE,
+      image: obj.image >= param.image,
+    };
+  },
+  '~': (obj, param) => {
+    return {
+      name: 'NumberLiteralRange',
+      start: param.image,
+      end: obj.image,
+    };
+  },
+  to_s: (obj, param) => {
+    return String(obj.image);
+  },
+};
