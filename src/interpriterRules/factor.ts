666f7d2377861f1b6805d81003666953e24e9560
su-u <aaaiiiuuu35@gmail.com>
2020-10-24 03:36:07 +0900

    Feat: 分離

--- src/interpriterRules/factor.ts
+++ src/interpriterRules/factor.ts
@@ -1,6 +1,8 @@
 import { IInterpreterRules } from './types';
-import { VariableManager } from '../manager';
 import { LiteralTokens } from '../tokens';
+import { callFunction } from "./factor/callFunction";
+import { identifier } from "./factor/identifier";
+import { booleanLiteral } from "./factor/boolLiteral";
 
 export const factor = ({ ast, manager, execObject }: IInterpreterRules) => {
   if (ast.children.NumberLiteral !== undefined) {
@@ -10,50 +12,11 @@ export const factor = ({ ast, manager, execObject }: IInterpreterRules) => {
       image,
     };
   } else if (ast.children.Identifier !== undefined) {
-    return manager.variable.reference(ast.children.Identifier[0].image);
+    return identifier({ ast, manager, execObject })
   } else if (ast.children.CallFunction !== undefined) {
-    const obj = ast.children.CallFunction[0];
-    const name = obj.children.FunctionNameToken[0].image.slice(0, -1);
-    const functionData = manager.function.reference(name);
-    if (functionData.type === 'user') {
-      const scopeManger = new VariableManager(manager.variable);
-      functionData.arguments.forEach((x: any, i: number) => {
-        scopeManger.assignment(
-          x,
-          execObject.interpreter({
-            ast: obj.children.arguments[0].children.Factor[i],
-            manager: {
-              variable: scopeManger,
-              function: manager.function,
-            },
-            execObject,
-          }),
-        );
-      });
-      return execObject.interpreter({
-        ast: functionData.function,
-        manager: {
-          variable: scopeManger,
-          function: manager.function,
-        },
-        execObject,
-      });
-    } else {
-      const arg = functionData.arguments.map((_: any, i: number) => {
-        return execObject.interpreter({
-            ast: obj.children.arguments[0].children.Factor[i],
-            manager,
-            execObject,
-          });
-      });
-      return functionData.function(arg);
-    }
+    return callFunction({ ast, manager, execObject });
   } else if (ast.children.BoolLiteral !== undefined) {
-    const image = ast.children.BoolLiteral[0].image;
-    return {
-      name: LiteralTokens.BooleanLiteral,
-      image,
-    };
+    return booleanLiteral({ ast, manager, execObject })
   } else if (ast.children.StringLiteral !== undefined) {
     const image = ast.children.StringLiteral[0].image.slice(1).slice(0, -1);
     return {
---
+++ src/interpriterRules/factor/callFunction.ts
@@ -0,0 +1,42 @@
+import { IInterpreterRules } from "../types";
+import { VariableManager } from "../../manager";
+
+
+export const callFunction = ({ ast, manager, execObject }: IInterpreterRules) => {
+  const obj = ast.children.CallFunction[0];
+  const name = obj.children.FunctionNameToken[0].image.slice(0, -1);
+  const functionData = manager.function.reference(name);
+  if (functionData.type === 'user') {
+    const scopeManger = new VariableManager(manager.variable);
+    functionData.arguments.forEach((x: any, i: number) => {
+      scopeManger.assignment(
+        x,
+        execObject.interpreter({
+          ast: obj.children.arguments[0].children.Factor[i],
+          manager: {
+            variable: scopeManger,
+            function: manager.function,
+          },
+          execObject,
+        }),
+      );
+    });
+    return execObject.interpreter({
+      ast: functionData.function,
+      manager: {
+        variable: scopeManger,
+        function: manager.function,
+      },
+      execObject,
+    });
+  } else {
+    const arg = functionData.arguments.map((_: any, i: number) => {
+      return execObject.interpreter({
+        ast: obj.children.arguments[0].children.Factor[i],
+        manager,
+        execObject,
+      });
+    });
+    return functionData.function(arg);
+  }
+}
