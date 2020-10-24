e3ec674541ac36ea9b6ea2b98863a0b7125aeffb
su-u <aaaiiiuuu35@gmail.com>
2020-10-24 22:23:17 +0900

    Fix: bug

--- src/interpriterRules/functions.ts
+++ src/interpriterRules/functions.ts
@@ -2,9 +2,10 @@ import { IInterpreterRules } from './types';
 
 export const functionStatement = ({ ast, manager }: IInterpreterRules) => {
   const functionName = ast.children.FunctionNameToken[0].image.slice(0, -1);
-  const funcArguments = ast.children.arguments[0].children.Identifier.map((x: any) => {
+  const argAst = ast.children.arguments[0];
+  const funcArguments =argAst.children.Identifier !== undefined ? argAst.children.Identifier.map((x: any) => {
     return x.image;
-  });
+  }) : [];
   manager.function.assignment(functionName, funcArguments, ast.children.Program[0]);
   return;
 };
