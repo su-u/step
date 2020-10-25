3509769f3cb0cf2a7fc841cbfa7eab7a70ab660d
su-u <aaaiiiuuu35@gmail.com>
2020-10-26 07:29:26 +0900

    Feat: orの実装

--- src/class/booleanClass.ts
+++ src/class/booleanClass.ts
@@ -1,9 +1,11 @@
 import { LiteralType } from '../types/literal';
 import { LiteralTokens } from '../tokens';
+import { BooleanLiteralTokens } from '../tokens';
 
 export type NumberObjectMethodType = {
   '=': (obj: LiteralType<boolean>, param: any) => any;
-  'and': (obj: LiteralType<boolean>, param: any) => any;
+  and: (obj: LiteralType<boolean>, param: any) => any;
+  or: (obj: LiteralType<boolean>, param: any) => any;
 };
 
 export const BooleanClass: NumberObjectMethodType = {
@@ -13,10 +15,22 @@ export const BooleanClass: NumberObjectMethodType = {
       image: String(obj.image === param.image),
     };
   },
-  'and': (obj, param) => {
+  and: (obj, param) => {
     return {
       name: LiteralTokens.BooleanLiteral,
-      image: String(obj.image === param.image),
+      image: String(
+        // @ts-ignore
+        obj.image === BooleanLiteralTokens.true && param.image === BooleanLiteralTokens.true,
+      ),
+    };
+  },
+  or: (obj, param) => {
+    return {
+      name: LiteralTokens.BooleanLiteral,
+      image: String(
+        // @ts-ignore
+        obj.image === BooleanLiteralTokens.true || param.image === BooleanLiteralTokens.true,
+      ),
     };
   },
 };
