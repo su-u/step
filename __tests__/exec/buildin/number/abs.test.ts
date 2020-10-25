fa4c3ac9b994a7ba2905c66ab6e72d22452dd203
su-u <aaaiiiuuu35@gmail.com>
2020-10-25 15:31:15 +0900

    Create abs.test.ts

---
+++ __tests__/exec/buildin/number/abs.test.ts
@@ -0,0 +1,69 @@
+import { exec } from '@/util/test';
+import { VariableManager } from '@/manager';
+import { FunctionManager } from '@/manager';
+import { LiteralTokens } from '@/tokens';
+
+describe('condition number abs', () => {
+  let manager = null;
+  beforeEach(() => {
+    manager = {
+      variable: new VariableManager(null),
+      function: new FunctionManager(),
+    };
+  });
+
+  test('1', () => {
+    const source = `
+1.5 |> abs -> num
+    `;
+    const resultManager = exec(source, manager).variable;
+    expect(resultManager.reference('num')).toStrictEqual({
+      name: LiteralTokens.NumberLiteral,
+      image: 1.5,
+    });
+  });
+
+  test('2', () => {
+    const source = `
+1 |> abs -> num
+    `;
+    const resultManager = exec(source, manager).variable;
+    expect(resultManager.reference('num')).toStrictEqual({
+      name: LiteralTokens.NumberLiteral,
+      image: 1,
+    });
+  });
+
+  test('3', () => {
+    const source = `
+-1.9 |> abs -> num
+    `;
+    const resultManager = exec(source, manager).variable;
+    expect(resultManager.reference('num')).toStrictEqual({
+      name: LiteralTokens.NumberLiteral,
+      image: 1.9,
+    });
+  });
+
+  test('4', () => {
+    const source = `
+-1500 |> abs -> num
+    `;
+    const resultManager = exec(source, manager).variable;
+    expect(resultManager.reference('num')).toStrictEqual({
+      name: LiteralTokens.NumberLiteral,
+      image: 1500,
+    });
+  });
+
+  test('4', () => {
+    const source = `
+100 - 1500 |> abs -> num
+    `;
+    const resultManager = exec(source, manager).variable;
+    expect(resultManager.reference('num')).toStrictEqual({
+      name: LiteralTokens.NumberLiteral,
+      image: 1400,
+    });
+  });
+});
