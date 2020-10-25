0714b1610852acab0988c23d5447c0dee86a945e
su-u <aaaiiiuuu35@gmail.com>
2020-10-25 16:09:41 +0900

    Feat: rangeを出力できるように

--- src/buildInFunctions/consoleFunctions.ts
+++ src/buildInFunctions/consoleFunctions.ts
@@ -4,11 +4,16 @@ import { IFunction } from './type';
 
 export const ConsoleBuildInFunctions: { [key: string]: IFunction } = {
   console: {
-    function: (obj: AllLiteralType[]) => {
-      if (Array.isArray(obj)) {
+    function: (obj: AllLiteralType[] | any) => {
+      if (obj.length > 1) {
         console.log(...obj.map((x) => x.image));
+      } else if (obj[0].name === LiteralTokens.NumberLiteralRange) {
+          const range = Array.from(Array(obj[0].end - obj[0].start + 1).keys(), (x) => x + obj[0].start);
+          range.forEach((i) => {
+            console.log(i);
+          });
       } else {
-        console.log((obj as any).image);
+          console.log((obj[0] as any).image);
       }
       return {
         name: LiteralTokens.StringLiteral,
