2a0a652dd92415f251388fb9052db3ec1d2428ee
su-u <aaaiiiuuu35@gmail.com>
2020-10-26 20:21:22 +0900

    Fix: index取得のast修正

--- src/interpriterRules/factor/arrayElement.ts
+++ src/interpriterRules/factor/arrayElement.ts
@@ -3,6 +3,6 @@ import { getIndex } from '../../util/ast';
 
 export const arrayElement = ({ ast, manager, execObject }: IInterpreterRules) => {
   const variableName = ast.children.ArrayElement[0].children.IdentifierSuffix[0].image.slice(0, -1);
-  const index = getIndex({ ast, manager, execObject });
+  const index = getIndex({ ast: ast.children.ArrayElement[0].children.index[0], manager, execObject });
   return manager.variable.reference(variableName, index);
 };
