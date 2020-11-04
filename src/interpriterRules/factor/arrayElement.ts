f0b8c9ab47baa9679f15db143914d7ed5b0d5024
su-u <aaaiiiuuu35@gmail.com>
2020-11-05 07:25:11 +0900

    format

--- src/interpriterRules/factor/arrayElement.ts
+++ src/interpriterRules/factor/arrayElement.ts
@@ -3,6 +3,10 @@ import { getIndex } from '../../util/ast';
 
 export const arrayElement = ({ ast, manager, execObject }: IInterpreterRules) => {
   const variableName = ast.children.ArrayElement[0].children.IdentifierSuffix[0].image.slice(0, -1);
-  const index = getIndex({ ast: ast.children.ArrayElement[0].children.index[0], manager, execObject });
+  const index = getIndex({
+    ast: ast.children.ArrayElement[0].children.index[0],
+    manager,
+    execObject,
+  });
   return manager.variable.reference(variableName, index);
 };
