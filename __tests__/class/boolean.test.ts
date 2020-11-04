f0f1cb62dddf21d87aafa8f5c757c40a6f18c3c1
su-u <aaaiiiuuu35@gmail.com>
2020-11-05 07:25:23 +0900

    Feat: add tests

---
+++ __tests__/class/boolean.test.ts
@@ -0,0 +1,172 @@
+import { LiteralTokens } from '@/tokens';
+import { BooleanClass } from '@/class/booleanClass';
+
+const func = (obj1, obj2, method) => {
+  return BooleanClass[method](obj1, obj2);
+};
+
+const TRUE = {
+  name: LiteralTokens.BooleanLiteral,
+  image: 'true',
+};
+
+const FALSE = {
+  name: LiteralTokens.BooleanLiteral,
+  image: 'false',
+};
+
+describe('BooleanClass', () => {
+  describe('=', () => {
+    test('1', () => {
+      const result = func(
+        {
+          name: LiteralTokens.BooleanLiteral,
+          image: 'true',
+        },
+        {
+          name: LiteralTokens.BooleanLiteral,
+          image: 'true',
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(TRUE);
+    });
+
+    test('2', () => {
+      const result = func(
+        {
+          name: LiteralTokens.BooleanLiteral,
+          image: 'true',
+        },
+        {
+          name: LiteralTokens.BooleanLiteral,
+          image: 'false',
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(FALSE);
+    });
+
+    test('3', () => {
+      const result = func(
+        {
+          name: LiteralTokens.NumberLiteral,
+          image: 1,
+        },
+        {
+          name: LiteralTokens.NumberLiteral,
+          image: 1,
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(TRUE);
+    });
+
+    test('4', () => {
+      const result = func(
+        {
+          name: LiteralTokens.NumberLiteral,
+          image: 1,
+        },
+        {
+          name: LiteralTokens.NumberLiteral,
+          image: 2,
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(FALSE);
+    });
+
+    test('5', () => {
+      const result = func(
+        {
+          name: LiteralTokens.StringLiteral,
+          image: '1',
+        },
+        {
+          name: LiteralTokens.StringLiteral,
+          image: '1',
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(TRUE);
+    });
+
+    test('6', () => {
+      const result = func(
+        {
+          name: LiteralTokens.NumberLiteral,
+          image: 1,
+        },
+        {
+          name: LiteralTokens.StringLiteral,
+          image: '1',
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(FALSE);
+    });
+
+    test('7', () => {
+      const result = func(
+        {
+          name: LiteralTokens.StringLiteral,
+          image: '1',
+        },
+        {
+          name: LiteralTokens.NumberLiteral,
+          image: 1,
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(FALSE);
+    });
+
+    test('8', () => {
+      const result = func(
+        {
+          name: LiteralTokens.StringLiteral,
+          image: 'true',
+        },
+        {
+          name: LiteralTokens.BooleanLiteral,
+          image: 'true',
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(FALSE);
+    });
+
+    test('9', () => {
+      const result = func(
+        {
+          name: LiteralTokens.BooleanLiteral,
+          image: 'false',
+        },
+        {
+          name: LiteralTokens.StringLiteral,
+          image: 'false',
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(FALSE);
+    });
+
+    test('10', () => {
+      const result = func(
+        {
+          name: LiteralTokens.NumberLiteralRange,
+          start: 0,
+          end: 10,
+        },
+        {
+          name: LiteralTokens.NumberLiteralRange,
+          start: 0,
+          end: 10,
+        },
+        '=',
+      );
+      expect(result).toStrictEqual(TRUE);
+    });
+  });
+});
