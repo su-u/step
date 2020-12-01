4c5b77946254dc3e1a7b19e1d162095ee19f5b55
su-u <aaaiiiuuu35@gmail.com>
2020-12-01 09:21:50 +0900

    Refactor: 関数分離

--- src/interpriterRules/pipe.ts
+++ src/interpriterRules/pipe.ts
@@ -1,75 +1,16 @@
 import { IInterpreterRules } from './types';
-import { VariableManager } from '../manager';
-import { LiteralTokens } from '../tokens';
-import { TypeError } from '../error';
+import { execEach } from './pipe/each';
+import { execFunction } from './pipe/function';
 
 export const pipe = ({ ast, manager, execObject }: IInterpreterRules) => {
   const childrenAst = ast.children.from[0];
   const tail = ast.children.tail[0];
-  // console.log(childrenAst);
   const value = execObject.interpreter({ ast: childrenAst, manager, execObject });
   if (tail.children.PipeToken !== undefined) {
     if (tail.children.toEach !== undefined) {
-      // each
-      if (value.start === undefined) throw new TypeError(value.name);
-      const eachObj = tail.children.toEach[0];
-      const range = Array.from(Array(value.end - value.start + 1).keys(), (x) => x + value.start);
-      range.forEach((i) => {
-        const inManger = {
-          variable: new VariableManager(manager.variable),
-          function: manager.function,
-        };
-        if (eachObj.children.Identifier !== undefined) {
-          inManger.variable.assignment(eachObj.children.Identifier[0].image, {
-            name: LiteralTokens.NumberLiteral,
-            image: i,
-          });
-        }
-        return execObject.interpreter({
-          ast: eachObj.children.Program[0],
-          manager: inManger,
-          execObject,
-        });
-      });
-      return;
+      return execEach({ ast, manager, execObject });
     } else if (tail.children.toIdentifier !== undefined) {
-      // 関数実行
-      let last = value;
-      Object.values(tail.children.toIdentifier).forEach((x: any, i: number) => {
-        const objName = x.image;
-        const functionData = manager.function.reference(objName);
-        const literals = Array.isArray(last) ? last : [last];
-        if (functionData.type === 'user') {
-          const scopeManger = new VariableManager(manager.variable);
-          functionData.arguments.forEach((argName: any, i: number) => {
-            let argValue: null;
-            if (literals.find((literal) => literal.name === argName)) {
-              argValue = literals.find((literal) => literal.name === argName).value;
-            } else {
-              argValue = literals[i].value !== undefined ? literals[i].value : literals[i];
-            }
-
-            scopeManger.assignment(argName, argValue);
-          });
-          last = execObject.interpreter({
-            ast: functionData.function,
-            manager: {
-              variable: scopeManger,
-              function: manager.function,
-            },
-            execObject,
-          });
-        } else {
-          const arg = functionData.arguments
-            .map((_: any, i: number) => {
-              const image = literals[i].value !== undefined ? literals[i].value : literals[i];
-              return image;
-            })
-            .filter((x: any) => x !== undefined);
-          last = functionData.function(arg);
-        }
-      });
-      return last;
+      return execFunction({ ast, manager, execObject });
     }
   }
   return value;
---
+++ src/interpriterRules/pipe/each.ts
@@ -0,0 +1,32 @@
+import { IInterpreterRules } from '../types';
+import { TypeError } from "@/error";
+import { VariableManager } from "@/manager";
+import { LiteralTokens } from "@/tokens";
+
+export const execEach = ({ ast, manager, execObject }: IInterpreterRules) => {
+  const childrenAst = ast.children.from[0];
+  const tail = ast.children.tail[0];
+  const value = execObject.interpreter({ ast: childrenAst, manager, execObject });
+  // each
+  if (value.start === undefined) throw new TypeError(value.name);
+  const eachObj = tail.children.toEach[0];
+  const range = Array.from(Array(value.end - value.start + 1).keys(), (x) => x + value.start);
+  range.forEach((i) => {
+    const inManger = {
+      variable: new VariableManager(manager.variable),
+      function: manager.function,
+    };
+    if (eachObj.children.Identifier !== undefined) {
+      inManger.variable.assignment(eachObj.children.Identifier[0].image, {
+        name: LiteralTokens.NumberLiteral,
+        image: i,
+      });
+    }
+    return execObject.interpreter({
+      ast: eachObj.children.Program[0],
+      manager: inManger,
+      execObject,
+    });
+  });
+  return;
+};
