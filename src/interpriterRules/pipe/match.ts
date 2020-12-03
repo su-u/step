cf46ced866f751acba6cb142e9d595fd51df2bc6
su-u <aaaiiiuuu35@gmail.com>
2020-12-03 10:04:36 +0900

    Feat: デフォルト実行実装

--- src/interpriterRules/pipe/match.ts
+++ src/interpriterRules/pipe/match.ts
@@ -10,21 +10,32 @@ export const execMatch = ({ ast, manager, execObject }: IInterpreterRules) => {
   const value = execObject.interpreter({ ast: childrenAst, manager, execObject });
   const matchAst = tail.children.toMatch[0];
   const matchList = matchAst.children.MatchExpression.map((expression) => {
-    const conditions = execObject.interpreter({
-      ast: expression.children.LogicExpression[0],
-      manager,
-      execObject,
-    });
+    const conditions = getConditions({ ast: expression, manager, execObject });
     const program = expression.children.Program[0];
     return {
       conditions,
       program,
     };
   });
-  const m = matchList.find((expression) => _.isEqual(expression.conditions, value)) || null;
+  const m =
+    matchList.find(
+      (expression) => _.isEqual(expression.conditions, value) || expression.conditions === null,
+    ) || null;
 
   // console.log('v', value);
   // console.log('c', matchList);
   // console.log('m', m);
   return m === null ? value : execObject.interpreter({ ast: m.program, manager, execObject });
+};
+
+const getConditions = ({ ast, manager, execObject }: IInterpreterRules) => {
+  if (ast.children.logicExpression !== undefined) {
+    return execObject.interpreter({
+      ast: ast.children.LogicExpression[0],
+      manager,
+      execObject,
+    });
+  } else {
+    return null;
+  }
 };
