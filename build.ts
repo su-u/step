32280bc43b61d960d0398bc9f9d2358a1c0937da
su-u <aaaiiiuuu35@gmail.com>
2021-02-12 02:05:00 +0900

    ビルド関係修正

--- build.ts
+++ build.ts
@@ -1,17 +1,37 @@
 const { argv } = require('process')
 const { build } = require('esbuild')
 const path = require('path')
+const sass = require('sass');
+
+const sassPlugin = options => ({
+  name: 'esbuild-plugin-sass',
+  setup(build) {
+    build.onLoad({ filter: /\.s[ac]ss$/ }, ({ path }) => {
+      return new Promise(resolve => {
+        sass.render({ ...options, file: path }, (err, result) => {
+          resolve({
+            contents: result?.css,
+            loader: 'css',
+            errors: err ? [{ text: err.message }] : undefined,
+          })
+        })
+      })
+    })
+  },
+})
+
 // optionsの定義
 const options = {
   // 以下のdefineプロパティを設定しない場合Reactのプロジェクトの実行時にエラーが出ます
   define: { 'process.env.NODE_ENV': process.env.NODE_ENV },
-  entryPoints: [path.resolve(__dirname, 'src/web.ts')],
+  entryPoints: [path.resolve(__dirname, 'webSrc/index.tsx')],
   minify: argv[2] === 'production',
   bundle: true,
   target: 'es2016',
   platform: 'browser',
   outdir: path.resolve(__dirname, 'dist'),
-  tsconfig: path.resolve(__dirname, 'tsconfig.json')
+  tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+  plugins: [sassPlugin({})],
 }
 // Buildの実行
 build(options).catch(err => {
