d32da26d9528fd8d26da69ff7421fec6a2bfd280
su-u <aaaiiiuuu35@gmail.com>
2021-02-17 00:48:32 +0900

    配列を出力できるように

--- src/buildInFunctions/consoleFunctions.ts
+++ src/buildInFunctions/consoleFunctions.ts
@@ -4,7 +4,7 @@ import { IFunction } from './type';
 export const ConsoleBuildInFunctions: { [key: string]: IFunction } = {
   console: {
     function: (obj: any[] | any) => {
-      if (obj.length > 1) {
+      if (obj.length >= 2) {
         console.log(...obj.map((x) => x.image));
       } else if (obj[0].name === LiteralTokens.NumberLiteralRange) {
         const range = Array.from(
@@ -14,6 +14,8 @@ export const ConsoleBuildInFunctions: { [key: string]: IFunction } = {
         range.forEach((i) => {
           console.log(i);
         });
+      } else if (obj[0].name === LiteralTokens.ArrayLiteral) {
+        obj[0].image.map((x) => console.log(x.image));
       } else {
         console.log((obj[0] as any).image);
       }
