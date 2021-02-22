7fe0a5af7548afae21fc70a23047d873bdd18485
su-u <aaaiiiuuu35@gmail.com>
2021-02-23 01:17:59 +0900

    Feat: 型wip

---
+++ src/types/ast.ts
@@ -0,0 +1,54 @@
+declare type Program = {
+  name: 'Program';
+  children: {
+    ProgramRule: ProgramRule[];
+  }
+}
+
+declare type ProgramRule = {
+  name: 'ProgramRule';
+  children: {
+    rule: Rule[];
+  }
+}
+
+declare type Rule = {
+
+}
+
+declare type Assignment = {
+  name: 'Assignment';
+  children: {
+    ToRight: ToRight[]
+  }
+}
+
+declare type ToRight = {
+  name: 'ToRight';
+  children: {
+    from: LogicExpression[];
+  }
+}
+
+declare type LogicExpression = {
+  name: 'LogicalExpression';
+  children: {
+    RelationExpression: RelationExpression[];
+  }
+}
+
+declare type RelationExpression = {
+  name: 'RelationExpression';
+  children: {
+    Expression: Expression[];
+  }
+}
+
+declare type Expression = {
+  name: 'Expression';
+  Term: Term[];
+}
+
+declare type Term = {
+
+}
