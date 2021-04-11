71f31d780be197268c03ba809ade5f06d72a4d05
su-u <aaaiiiuuu35@gmail.com>
2021-04-11 09:41:53 +0900

    Feat: reactのアップデート

--- webSrc/Root.tsx
+++ webSrc/Root.tsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { Navigator } from './Navigator';
 import './css/root.scss';
 
-export const Root: React.FC = () => <Navigator />;
+export const Root: React.VFC = () => <Navigator />;
