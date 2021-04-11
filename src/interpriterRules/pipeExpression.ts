4b49b82357161bb9cd2ba7e85dc83dbe0e91f0ab
su-u <aaaiiiuuu35@gmail.com>
2021-04-11 10:54:43 +0900

    Update pipeExpression.ts

--- src/interpriterRules/pipeExpression.ts
+++ src/interpriterRules/pipeExpression.ts
@@ -6,16 +6,16 @@ import { execMatch } from './pipe/match';
 export const pipeExpression = ({ ast, execObject }: IInterpreterRules<any>) => {
   const fromAst = ast.children.head[0];
   let fromValue = execObject.interpreter({ ast: fromAst, execObject });
-  if (ast.children.tail !== undefined) {
+  if (ast.children.tail) {
     fromValue = ast.children.tail.reduce((last, pipeAst) => {
       const pipeExpressionAst = pipeAst.children.rules[0].children;
       // console.log(pipeExpressionAst);
       // console.log(fromValue);
-      if (pipeExpressionAst.toEach !== undefined) {
+      if (pipeExpressionAst.toEach) {
         return execEach({ ast: pipeExpressionAst, execObject }, last);
-      } else if (pipeExpressionAst.DotsIdentifier !== undefined) {
+      } else if (pipeExpressionAst.DotsIdentifier) {
         return execFunction({ ast: pipeExpressionAst, execObject }, last);
-      } else if (pipeExpressionAst.toMatch !== undefined) {
+      } else if (pipeExpressionAst.toMatch) {
         return execMatch({ ast: pipeExpressionAst, execObject }, last);
       }
     }, fromValue);
