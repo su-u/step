f15b0b0d50f21a3c8ec09137593ac865fe883d5d
su-u <aaaiiiuuu35@gmail.com>
2021-08-21 21:50:06 +0900

    Feat: build周り

--- packages/step-core/build.ts
+++ packages/step-core/build.ts
@@ -1,46 +1,20 @@
 const { argv } = require('process');
 const { build } = require('esbuild');
 const path = require('path');
-const dts = require('dts-bundle');
-// const sass = require('sass');
 
-// const sassPlugin = (options) => ({
-//   name: 'esbuild-plugin-sass',
-//   setup(build) {
-//     build.onLoad({ filter: /\.s[ac]ss$/ }, ({ path }) => {
-//       return new Promise((resolve) => {
-//         sass.render({ ...options, file: path }, (err, result) => {
-//           resolve({
-//             contents: result?.css,
-//             loader: 'css',
-//             errors: err ? [{ text: err.message }] : undefined,
-//           });
-//         });
-//       });
-//     });
-//   },
-// });
-//
-// const options = {
-//   // 以下のdefineプロパティを設定しない場合Reactのプロジェクトの実行時にエラーが出ます
-//   define: { 'process.env.NODE_ENV': process.env.NODE_ENV ?? 'production' },
-//   entryPoints: [path.resolve(__dirname, 'src/index.ts')],
-//   minify: argv[2] === 'production',
-//   bundle: true,
-//   target: 'es2016',
-//   platform: 'browser',
-//   outdir: path.resolve(__dirname, 'dist'),
-//   tsconfig: path.resolve(__dirname, '../../tsconfig.json'),
-//   // plugins: [sassPlugin({})],
-// };
-//
-// build(options).catch((err) => {
-//   process.stderr.write(err.stderr);
-//   process.exit(1);
-// });
+const options = {
+  // 以下のdefineプロパティを設定しない場合Reactのプロジェクトの実行時にエラーが出ます
+  define: { 'process.env.NODE_ENV': process.env.NODE_ENV ?? 'production' },
+  entryPoints: [path.resolve(__dirname, 'src/index.ts')],
+  minify: argv[2] === 'production',
+  bundle: true,
+  target: 'es2016',
+  platform: 'browser',
+  outdir: path.resolve(__dirname, 'dist'),
+  tsconfig: path.resolve(__dirname, '../../tsconfig.json'),
+};
 
-dts.bundle({
-  name: 'step-core',
-  main: './build/**/*.d.ts',
-  out: 'dist/index.d.ts'
+build(options).catch((err) => {
+  process.stderr.write(err.stderr);
+  process.exit(1);
 });
